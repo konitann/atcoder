8dcdc17911da84f3fa377c2d7745af08c50baa22
Konitann <m20046@g.metro-cit.ac.jp>
2024-06-17 19:45:30 +0900

    result is ac

--- 081B-ShiftOnly.cpp
+++ 081B-ShiftOnly.cpp
@@ -1,17 +1,18 @@
+//解法1.2進数にして、msbから数えて最初の0までの数を数える
+//解法2.2で割れるところまで割る(再帰関数使用)
 #include <iostream>
 #include <vector>
 using namespace std;
-int div(vector<int> vec){
-    for(int i = 0; i < n; i++){
-        if(vec[i] % 2 == 0)
-    }
-    return vec[0] + vec[2];
-}
+int div(int n, vector<int>& vec);
 int main(void){
-    // Your code here!
     int n;
     cin >> n;
     vector<int> vec(n);
     for(int i = 0; i < n; i++)cin >> vec[i];
-    cout << div(vec) << endl;
+    cout << div(n, vec) << endl;
+}
+int div(int n, vector<int>& vec){
+    for(int i = 0; i < n; i++)if(vec[i] % 2 != 0)return 0;
+    for(int i = 0; i < n; i++)vec[i]/=2;
+    return 1 + div(n,vec);
 }
